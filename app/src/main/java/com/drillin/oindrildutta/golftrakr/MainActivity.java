48ee60f591c5c639cb66b258595a8382037be953
duttaoindril <duttaoindril@gmail.com>
2015-12-25 22:36:36 -0800

    Fixed a few small bugz

--- app/src/main/java/com/drillin/oindrildutta/golftrakr/MainActivity.java
+++ app/src/main/java/com/drillin/oindrildutta/golftrakr/MainActivity.java
@@ -5,12 +5,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends ListActivity { //AppCompatActivity {
     private static final String PREFS_FILE = " com.drillin.golftrakrapp.preferences";
@@ -41,6 +36,7 @@ public class MainActivity extends ListActivity { //AppCompatActivity {
 
         sharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
         prefEditor = sharedPreferences.edit();
+        prefEditor.apply();
 
         for(int i = 0; i < holes.length; i++)
             holes[i] = sharedPreferences.getInt(KEY_NAME+i, 0);
