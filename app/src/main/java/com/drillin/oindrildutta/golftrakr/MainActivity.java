5a840ea93896754943b5b5cc8eb34b5d08f42822
duttaoindril <duttaoindril@gmail.com>
2015-12-25 22:33:29 -0800

    Added in new icons

--- app/src/main/java/com/drillin/oindrildutta/golftrakr/MainActivity.java
+++ app/src/main/java/com/drillin/oindrildutta/golftrakr/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends ListActivity { //AppCompatActivity {
     private ListAdapter lAdapter;
     private int[] holes = new int[18];
 
+    //https://romannurik.github.io/AndroidAssetStudio/icons-launcher.html#foreground.type=image&foreground.space.trim=0&foreground.space.pad=0.1&foreColor=fff%2C0&crop=0&backgroundShape=circle&backColor=2196f3%2C100&effects=shadow
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
